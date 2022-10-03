8eb380921ddb00af1b5a5a8382e87ad9fea03c1d
Abhinav Mishra <abhinavm65@gmail.com>
2022-10-04 01:10:54 +0530

    socketio server creation install socket modules

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(4000, () => {
+    console.log('Server started on port 3000');
+})
+
+app.use(express.static('public'))
+
+var io = socket(server,{
+    allowEIO3: true
+});
+
+io.on('connection', socket => {
+    console.log(`Made connection with Id ${socket.id}`);
+})
