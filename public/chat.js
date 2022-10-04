31993230d515fc6c1910654faa7c66ce52c1550a
Abhinav Mishra <abhinavm65@gmail.com>
2022-10-05 00:06:05 +0530

    broadcast message

--- public/chat.js
+++ public/chat.js
@@ -1,10 +1,11 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://192.168.1.35:4000');
 
 // Collect Input values
 let handle = document.getElementById('handle');
 let message = document.getElementById('message');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
+let feedback = document.getElementById('feedback');
 
 // emit data to server through socket
 btn.addEventListener('click',() => {
@@ -17,5 +18,14 @@ btn.addEventListener('click',() => {
 
 //handle event emitted from server
 socket.on('chat', (data) => {
+    feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing',handle.value);
+})
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 })
